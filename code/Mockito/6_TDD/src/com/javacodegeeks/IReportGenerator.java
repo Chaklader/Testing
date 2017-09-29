5d19e948f929a231f0cd1b97e86691eb942f8a7d
Chaklader <omi.chaklader@gmail.com>
2017-09-29 11:19:51 +0600

    updated

---
+++ code/Mockito/6_TDD/src/com/javacodegeeks/IReportGenerator.java
@@ -0,0 +1,14 @@
+package com.javacodegeeks;
+
+/**
+ * Interface for generating reports.
+ * @author Meraj
+ */
+public interface IReportGenerator {
+	
+	/**
+	 * Generate report.
+	 * @param report Report entity.
+	 */
+	void generateReport(ReportEntity report);
+}
