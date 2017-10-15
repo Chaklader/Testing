2eee9de474b6807543c46dbc9431681d62c970cb
Chaklader <omi.chaklader@gmail.com>
2017-10-15 16:43:22 +0600

    updated

---
+++ Code/Eugen/Mockito/src/test/java/com/baeldung/powermockito/introduction/CollaboratorWithFinalMethods.java
@@ -0,0 +1,8 @@
+package com.baeldung.powermockito.introduction;
+
+public class CollaboratorWithFinalMethods {
+
+    public final String helloMethod() {
+        return "Hello World!";
+    }
+}
--- code/Mockito/2_Stubbing/src/main/java/com/javacodegeeks/hughwphamill/mockito/stubbing/Printer.java
+++
@@ -1,7 +0,0 @@
-package com.javacodegeeks.hughwphamill.mockito.stubbing;
-
-public interface Printer {
-	
-	void printTestPage() throws PrinterNotConnectedException;
-
-}
